0fc4eb4f16c2237c39d3c8503658a0be3bc54f0d
squirrellover23 <petersonwingate@gmail.com>
2023-12-04 15:32:54 -0700

    fg

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const path = require('path');
 const bodyParser = require('body-parser');
-const ejs = require('ejs'); // Require EJS
+//const ejs = require('ejs'); // Require EJS
 
 // Initialize Express
 const app = express();
