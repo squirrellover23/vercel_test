4ab4384f9e5ede19462bbf87f54bbadf9496de5f
squirrellover23 <petersonwingate@gmail.com>
2023-12-04 15:53:22 -0700

    hmm ima confused again

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const express = require("express");
+const sqlite3 = require('sqlite3');
 
 const path = require('path');
 const bodyParser = require('body-parser');
 //const ejs = require('ejs'); // Require EJS
 
+const port = 5000;
 // Initialize Express
 const app = express();
-
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
 
-const port = 5000;
+
+// Configure SQLite database
+const db = new sqlite3.Database('./database.db');
+db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS names (firstName TEXT COLLATE NOCASE, lastName TEXT COLLATE NOCASE, visited INT DEFAULT 0, class TEXT COLLATE NOCASE, lastLoginTime INT DEFAULT 0)");
+    db.run("CREATE TABLE IF NOT EXISTS login_logs (id INTEGER PRIMARY KEY, user_id TEXT, login_time DATETIME);")
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Create GET request
 app.get("/", (req, res) => {
