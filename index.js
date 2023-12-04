b62a454bd63d9b19e5a15659a426211fa1818deb
squirrellover23 <petersonwingate@gmail.com>
2023-12-04 15:05:36 -0700

    u suck

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const path = require('path');
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 //const ejs = require('ejs'); // Require EJS
 
 // Initialize Express
