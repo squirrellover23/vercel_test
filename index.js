b1ebbaaa548b9e9c1d9f7b67b5c379246ec66bdb
squirrellover23 <petersonwingate@gmail.com>
2023-12-04 15:31:52 -0700

    no view

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const app = express();
 //app.use(bodyParser.urlencoded({ extended: true }));
 
 //Set the view engine to EJS
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, 'views'));
 
 //app.use(express.static(path.join(__dirname, 'public')));
 
