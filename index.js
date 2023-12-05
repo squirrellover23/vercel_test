a10fb07dd33a6cf58e277aeae2722a13dbad6395
squirrellover23 <petersonwingate@gmail.com>
2023-12-04 22:29:25 -0700

    Lots of stuff

--- index.js
+++ index.js
@@ -16,77 +16,244 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = 5000;
 
-async function makeRunQuery(query, retfunc){
-  try {
-    await sql`${query}`;
-    retfunc(null)
-  } catch (error) {
-    retfunc(error);
-    return;
-  }
-}
-
-async function makeRetQuery(query, retfunc){
-  try {
-    var response = await sql`${query}`;
-  } catch (error) {
-    retfunc(error, null);
-    return;
-  }
-  retfunc(null, response);
-
-}
-/*
-// Configure SQLite database
-const db = new sqlite3.Database('./database.db');
-db.serialize(() => {
-    db.run("CREATE TABLE IF NOT EXISTS names (firstName TEXT COLLATE NOCASE, lastName TEXT COLLATE NOCASE, visited INT DEFAULT 0, class TEXT COLLATE NOCASE, lastLoginTime INT DEFAULT 0)");
-    db.run("CREATE TABLE IF NOT EXISTS login_logs (id INTEGER PRIMARY KEY, user_id TEXT, login_time DATETIME);")
-});
 
 
-*/
 
 
-const createLoginsTableQ = `CREATE TABLE IF NOT EXISTS login_logs (user_id TEXT, login_time TIMESTAMP);`
-
-// Create GET request
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+    res.sendFile(path.join(__dirname, 'public/home.html'));
 });
+
 
 app.get("/createTable", async (req, res) =>{
-  try {
-    var response = await sql`DROP TABLE IF EXISTS names22`
-    res.status(200).json({ response })
+    try {
+        var response = await sql`DROP TABLE IF EXISTS names22`
+        res.status(200).json({ response })
 
-  } catch (error) {
-    res.status(500).send(err)
+    } catch (error) {
+        res.status(500).send(err)
 
-  }
-  /*
-  console.log('where is this ')
-  makeRetQuery(createNameQ, (err, ret) =>{
-    if (err){
-      res.status(500).send(err)
-      res.status(500).send(err)
-    } else {
-      console.log('this is good?')
-      res.status(200).json({ ret })
     }
-  })
-  */
+ 
 });
 
 
 
 app.get('/test', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/test.html'));
+    res.sendFile(path.join(__dirname, 'public/test.html'));
 });
+
+
+
+
+
+app.post('/updatevisit', async (req, res) => {
+    const { firstName, lastName, action } = req.body;
+    try {
+        var row = await sql`SELECT * FROM names WHERE firstName = ${firstName} AND lastName = ${lastName};`
+    } catch (error) {
+        res.status(500).send('Error checking user existence.');
+        return
+    } 
+    if (!row){
+        res.status(401).send('Invalid user information.');
+    } else {
+        const lastLoginTime = row.lastLoginTime || 0;
+        const currentTime = Date.now();
+
+        if (currentTime - lastLoginTime >= 10 * 3600 * 1000) {
+            // User can log in (it's been at least 5 minutes)
+            // Update the last login time
+            //AND SET visited = visited + 1
+            var error = true; 
+            db.run("UPDATE names SET lastLoginTime = ?, visited = visited + 1 WHERE firstName = ? AND lastName = ?", [currentTime, firstName, lastName], (err) => {
+                if (err) {
+                    res.status(500).send('Error updating last login time.');
+                    error = false;
+                } 
+            });
+            const currentDate = new Date().toISOString();
+            const userId = `${row.firstName} ${row.lastName}`;  // Assuming the user ID is in the "names" table
+            db.run("INSERT INTO login_logs (user_id, login_time) VALUES (?, ?)", [userId, currentDate], (err) => {
+                if (err) {
+                    res.status(500).send('Error loging visit.');
+                    error = false
+                } 
+            });
+            if (error) {
+                res.status(200).json({visits: row.visited})
+            }
+
+        } else {
+            res.status(429).send('You Already Logged For Today. Please try again later.');
+        }
+    }
+
+
+    db.get("SELECT * FROM names WHERE firstName = ? AND lastName = ?", [firstName, lastName], (err, row) => {
+        if (err) {
+            res.status(500).send('Error checking user existence.');
+        } else if (!row) {
+            res.status(401).send('Invalid user information.');
+        } else {
+            const lastLoginTime = row.lastLoginTime || 0;
+            const currentTime = Date.now();
+    
+            if (currentTime - lastLoginTime >= 10 * 3600 * 1000) {
+                // User can log in (it's been at least 5 minutes)
+                // Update the last login time
+                //AND SET visited = visited + 1
+                var error = true; 
+                db.run("UPDATE names SET lastLoginTime = ?, visited = visited + 1 WHERE firstName = ? AND lastName = ?", [currentTime, firstName, lastName], (err) => {
+                    if (err) {
+                        res.status(500).send('Error updating last login time.');
+                        error = false;
+                    } 
+                });
+                const currentDate = new Date().toISOString();
+                const userId = `${row.firstName} ${row.lastName}`;  // Assuming the user ID is in the "names" table
+                db.run("INSERT INTO login_logs (user_id, login_time) VALUES (?, ?)", [userId, currentDate], (err) => {
+                    if (err) {
+                        res.status(500).send('Error loging visit.');
+                        error = false
+                    } 
+                });
+                if (error) {
+                    res.status(200).json({visits: row.visited})
+                }
+
+            } else {
+                res.status(429).send('You Already Logged For Today. Please try again later.');
+            }
+        }
+      
+    }); 
+});
+
+/*
+
+
+app.post('/addnewuser', (req, res) => {
+    const { firstName, lastName, classIn } = req.body;
+
+    // Check if the user already exists in the database
+    db.get("SELECT * FROM names WHERE firstName = ? AND lastName = ?", [firstName, lastName], (err, row) => {
+        if (err) {
+            res.status(500).send('Error checking user existence.');
+        } else if (row) {
+            // User already exists, send an error message
+            //res.redirect('/error');
+        } else {
+            // User doesn't exist, insert into the database
+            db.run("INSERT INTO names (firstName, lastName, visited, class, lastLoginTime) VALUES (?, ?, 0, ?, 0)", [firstName, lastName, classIn], (err) => {
+                if (err) {
+                    res.status(500).send('Error adding the new user.');
+                } else {
+                    res.render('useradded', { first: firstName, last: lastName });
+
+                }
+            });
+        }
+    });
+});
+*/
+
+async function getUniqueValues(columnName, tableName, callback) {
+
+    // SQLite query to get unique values
+    try {
+        const response = await sql`SELECT DISTINCT ${columnName} FROM ${tableName}`;
+    } catch (error) {
+        callback(err, null);
+        return;
+    }
+    const uniqueValues = rows.map(row => row[columnName]);
+    // Return the unique values
+    callback(null, uniqueValues);
+
+}
+
+// Example usage
+
+
+app.get('/adduser', (req, res) => {
+    getUniqueValues('class', 'names', (err, result) => {
+        if (err) {
+            res.status(500).send('Error fetching class data.');
+        }
+        res.render('adduser', {classes: result});
+    });
+            
+});
+
+
+app.get('/viewusers', async (req, res) => {
+    try {
+        const rows = await sql`SELECT firstName, lastName, visited, class FROM names`;
+        res.render('viewusers', { users: rows});
+    } catch (error) {
+        res.status(500).send('Error fetching user data.');
+        return;
+    }
+    
+    
+});
+
+/*
+app.post('/updatevisits', (req, res) => {
+    const { firstName, lastName, visits } = req.body;
+    db.run("UPDATE names SET visited = ? WHERE firstName = ? AND lastName = ?", [visits, firstName, lastName], (err) => {
+        if (err) {
+            res.status(500).send('Error updating the visit count.');
+        } else {
+            res.redirect('/viewusers'); // Redirect back to the "View Users" page
+        }
+    });
+});
+
+
+app.post('/checkuser', (req, res) => {
+    const { firstName, lastName } = req.body;
+    db.get("SELECT * FROM names WHERE firstName = ? AND lastName = ?", [firstName, lastName], (err, row) => {
+        if (err) {
+            res.status(500).send('Error checking user existence.');
+        } else if (!row) {
+            res.status(401).send('Invalid user information.');
+        } else {
+            res.status(200).send('User exists in db')
+        }
+    });
+});
+
+
+app.post('/deleteuser', (req, res) => {
+    const { firstName, lastName } = req.body;
+    db.run("DELETE FROM names WHERE firstName = ? AND lastName = ?", [firstName, lastName], (err) => {
+        if (err) {
+            res.status(500).send('Error deleting the user.');
+        } else {
+            res.redirect('/viewusers'); // Redirect back to the "View Users" page
+        }
+    });
+});
+
+app.get('/login-attempts', (req, res) => {
+    // Retrieve all login attempts from the login_logs table
+    db.all('SELECT * FROM login_logs', (err, rows) => {
+      if (err) {
+        console.error('Error retrieving login attempts:', err.message);
+        res.status(500).send('Internal Server Error');
+      } else {
+        // Render the login attempts page and pass the login attempts data to the view
+        res.render('login-attempts', { loginAttempts: rows });
+      }
+    });
+});
+*/
 
 // Initialize server
 app.listen(port, () => {
-  console.log(`Running on port ${port}.`);
+    console.log(`Running on port ${port}.`);
 });
 
 module.exports = app;
